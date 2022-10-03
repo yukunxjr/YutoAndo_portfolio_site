0a32725f9895fe0fcb220664e7c66017b2bbeed8
yukunxjr <yukunxjr@yahoo.co.jp>
2022-10-03 14:18:07 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <script setup>
-import Footer from './components/Footer.vue';
-import Header from './components/Header.vue';
+import { ref } from 'vue';
+import AppHeader from './components/AppHeader.vue';
+
+const todos = ref([]);
+const newTodo = ref('');
+
+const add = () => {
+  todos.value.push(newTodo.value);
+  newTodo.value = '';
+};
+
+const removeTodo = (i) => {
+  todos.value.splice(i, 1);
+};
 </script>
 
 <template>
-  <Header />
-  <router-view></router-view>
-  <Footer />
+  <AppHeader x="red">My Todo</AppHeader>
+  <input type="text" v-model="newTodo" />
+  <button @click="add">追加</button>
+
+  <ul>
+    <li v-for="(todo, i) in todos" v-bind:key="i">
+      {{ todo }} <span @click="removeTodo"> 削除 </span>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps(['x']);
+</script>
+<template>
+  <header>
+    <h1 v-bind:style="{ color: x }"><slot></slot></h1>
+  </header>
+</template>
